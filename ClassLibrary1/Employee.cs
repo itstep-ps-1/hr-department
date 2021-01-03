b0503652269fba4a4d50980678f62cf4d5e964f1
teacher <itstep.codeclub@gmail.com>
2021-01-03 13:15:30 +0200

    first commit

---
+++ ClassLibrary1/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    class Employee : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private int age;
+        private string position;
+        private decimal salary;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                OnPropertyChanged("Position");
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                salary = value;
+                OnPropertyChanged("Salary");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
